9ce9fab47bb72c5d7c21cc601c3796cd92f1a13d
Nuottajarvi <peetu.nuottajarvi@gmail.com>
2017-11-25 03:03:17 +0200

    first commit

---
+++ Assets/Icecream.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Icecream : MonoBehaviour {
+
+	int height = 150;
+	int width = 150;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
