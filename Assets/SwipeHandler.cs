3d8f8a985fc59eeddc2a6682a24c8fd73d5fbf50
Luka Tsulaia <lucho.tsulaia@yahoo.com>
2017-11-25 17:06:34 +0200

    added swiping script

---
+++ Assets/SwipeHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SwipeHandler : Selectable, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Vector2 start;
+    Vector2 changeInThisFrame;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (!interactable)
+            return;
+        start = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        changeInThisFrame = eventData.position - start;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (!interactable)
+            return;
+		
+    }
+}
