bcc0eb2ac1be8e114b5ac2041bbff1c6b6ff3642
Nuottajarvi <peetu.nuottajarvi@gmail.com>
2017-11-25 03:40:26 +0200

    drop spawning script

---
+++ Assets/Melt.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melt : MonoBehaviour {
+
+	public GameObject drop;
+	public float cooldown;
+	private float timer;
+
+	void Start () {
+		timer = cooldown;
+	}
+	
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if(timer < 0) {
+			GameObject newdrop = Instantiate(drop, transform);
+
+			System.Random random = new System.Random();
+			int angle = random.Next (360);
+
+			var x = Mathf.Cos(angle) * 1.5f;
+			var z = Mathf.Sin(angle) * 1.5f;
+
+			newdrop.transform.position = new Vector3(x,0,z);
+			newdrop.transform.position = new Vector3(x,0,z);
+			newdrop.transform.LookAt(transform);
+
+			timer = cooldown;
+		}
+	}
+}
